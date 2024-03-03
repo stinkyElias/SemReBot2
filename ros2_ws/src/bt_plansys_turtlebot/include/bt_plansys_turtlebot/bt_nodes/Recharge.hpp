6797cc6419c0a062bbbce21ffdc6b303a422e995
stinkyElias <elias.kalland@outlook.com>
2024-03-03 19:50:52 +0100

    Navigate node working
    Former-commit-id: fee263c3fc3d8d85c5ab36595a23e3e3d99c2dba

---
+++ ros2_ws/src/bt_plansys_turtlebot/include/bt_plansys_turtlebot/bt_nodes/Recharge.hpp
@@ -0,0 +1,28 @@
+#ifndef RECHARGE_HPP
+#define RECHARGE_HPP
+
+#include <string>
+#include <iostream>
+
+#include "behaviortree_cpp/behavior_tree.h"
+#include "behaviortree_cpp/bt_factory.h"
+
+class Recharge: public BT::ActionNodeBase{
+    public:
+        explicit Recharge(
+            const std::string &xml_name,
+            const BT::NodeConfiguration &configuration
+        );
+
+        void halt();
+        BT::NodeStatus tick();
+
+        static BT::PortsList providedPorts(){
+            return BT::PortsList({});
+        }
+    
+    private:
+        int counter_;
+};
+
+#endif // RECHARGE_HPP
