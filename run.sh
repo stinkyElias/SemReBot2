74bda2a3002846e74876699d861926f22389e50b
stinkyElias <elias.kalland@outlook.com>
2024-03-06 13:07:59 +0100

    rewrote vscode setting files

--- run.sh
+++ run.sh
@@ -8,8 +8,10 @@ docker run -it \
     --env=DISPLAY \
     --env=QT_X11_NO_MITSHM=1 \
     -v /home/stinky/Documents/semantic-robot/ros2_ws/src/bt_plansys_turtlebot:/home/stinky/ros2_ws/src/bt_plansys_turtlebot \
-    -v /home/stinky/Documents/semantic-robot/ros2_ws/.vscode:/home/stinky/ros2_ws/.vscode \
+    -v /home/stinky/Documents/semantic-robot/.vscode:/home/stinky/.vscode \
     -v /home/stinky/.Xauthority:/home/stinky/.Xauthority:rw \
     -v /home/stinky/Documents/semantic-robot/env.sh:/home/stinky/ros2_ws/env.sh \
+    -v /home/stinky/Documents/semantic-robot/llm:/home/stinky/llm \
+    -v /home/stinky/Documents/semantic-robot/stt:/home/stinky/stt \
     plansys:full \
     /bin/bash
