f7bfa6c3de9e7ebb313322f201cf8ad63eb8e68c
stinkyElias <elias.kalland@outlook.com>
2024-03-11 20:29:07 +0100

    starting nlp pkg

--- env.sh
+++ env.sh
@@ -5,5 +5,5 @@ export TURTLEBOT3_MODEL=waffle
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/rolling/share/turtlebot3_gazebo/models
 
 source install/setup.bash
-colcon build --symlink-install --packages-select task_controller
+colcon build --symlink-install --packages-select task_controller nlp_handler
 source install/setup.bash
--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ docker run -it \
     --env=DISPLAY \
     --env=QT_X11_NO_MITSHM=1 \
     -v /home/stinky/Documents/semantic-robot/ros2_ws/src/task_controller:/home/stinky/ros2_ws/src/task_controller \
+    -v /home/stinky/Documents/semantic-robot/ros2_ws/src/nlp_handler:/home/stinky/ros2_ws/src/nlp_handler \
     -v /home/stinky/Documents/semantic-robot/.vscode:/home/stinky/.vscode \
     -v /home/stinky/.Xauthority:/home/stinky/.Xauthority:rw \
     -v /home/stinky/Documents/semantic-robot/env.sh:/home/stinky/ros2_ws/env.sh \
