760d4d01cf395ab0eac29c501bca6bba3ffbec28
stinkyElias <elias.kalland@outlook.com>
2024-03-14 15:52:36 +0100

    added mistral node
    Former-commit-id: e8a209869f28d03e73cf7b990ef2a2fddad68898

---
+++ llm/mistral/mistral_4b.py
@@ -0,0 +1,63 @@
+import os
+os.environ['HF_HOME']='/home/stinky/models'
+
+import torch
+torch.cuda.empty_cache()
+
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+
+device = 'cuda'
+model_id = 'mistralai/Mistral-7B-Instruct-v0.2'
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+nf4_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type='nf4',
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,
+)
+
+model_nf4 = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=nf4_config)
+
+question = 'Move the new pallet from the unload zone to shelf 2 and recharge at the charging station.'
+
+messages = [
+        {'role': 'user', 'content': 'You will be receiving a natural language input from me, and you will return a text based on the input and on the format that I will show you three examples of. You will act based on a PDDL domain file.'},
+        {'role': 'assistant', 'content': 'I understand. Give me the domain.pddl'},
+        {'role': 'user', 'content': '(define (domain warehouse)(:types robot zone pallet)(:predicates(robot_available ?robot - robot)(robot_at ?robot - robot ?zone - zone)(pallet_at ?pallet - pallet ?zone - zone)(is_pallet ?pallet - pallet)(pallet_not_moved ?pallet - pallet)(is_unload_zone ?zone - zone)(is_reol_zone ?zone - zone))'},
+        {'role': 'assistant', 'content': 'I see. This is the types and predicates in the domain.pddl.'},
+        {'role': 'user', 'content': 'Yes. I will show you an example, and you will see the format of the input and the output. Only answer in the same format as this example.'},
+        {'role': 'assistant', 'content': 'Understood, I will learn from the coming example and see how I shall respond. Only special characters I can use is "|", "(", ")" and "_".'},
+        {'role': 'user', 'content': 'Prompt: A new shipment arrived. Please move the new pallet from the unload zone to reol 1. Afterwards, wait at reol 2. -> Correct output: set instance pallet_1 pallet|set predicate pallet_at pallet_1 unload_zone|set predicate pallet_not_moved pallet_1|set goal pallet_at pallet_1 reol_1|set goal robot_at tars reol_2|'},
+        {'role': 'assistant', 'content': 'I understand the format. I will respond in the same format. Please give me the input.'},
+        {'role': 'user', 'content': question},
+    ]
+
+encodeds = tokenizer.apply_chat_template(messages, return_tensors='pt')
+
+model_inputs = encodeds.to(device)
+# model.to(device)
+
+generated_ids = model_nf4.generate(model_inputs, pad_token_id=tokenizer.eos_token_id, max_new_tokens=100, do_sample=True)
+decoded = tokenizer.batch_decode(generated_ids)
+
+# # Remove the pre-prompts and end-of-sentence token
+output_tokens = decoded[0]
+end_token = "[/INST]"
+
+# # Find last occurence of end_tag in output
+end_tag_index = output_tokens.rfind(end_token)
+end_of_sentence = -4
+sliced_output = output_tokens[end_tag_index + len(end_token):end_of_sentence]
+
+torch.cuda.empty_cache()
+
+delimiter = '|'
+
+# start from the back of sliced_output and find the last occurence of delimiter
+last_delimiter = sliced_output.rfind(delimiter)
+# slice the string from the last occurence of delimiter to the end
+sliced_output = sliced_output[:last_delimiter + 1]
+
+print(sliced_output)
--- ros2_ws/src/nlp_handler/nlp_handler/whisper_node.py
+++ ros2_ws/src/nlp_handler/nlp_handler/whisper_node.py
@@ -9,28 +9,49 @@ from std_msgs.msg import String, Int8
 from transformers import pipeline
 from transformers.utils import is_flash_attn_2_available
 
-class WhisperNode(Node):
-    # STT model parameters
-    model_name_='openai/whisper-large-v3'
-    device_='cuda:0' if torch.cuda.is_available() else 'cpu'
-    
+class WhisperNode(Node):   
     def __init__(self) -> None:
         super().__init__('whisper_node')
+
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('model_name', ''),
+                ('audio_dir', ''),
+                ('data_type', ''),
+                ('device', ''),
+                ('chunk_length', 0),
+                ('batch_size', 0)
+            ]
+        )
+
+        self.model_name_ = self.get_parameter('model_name').get_parameter_value().string_value
+        self.audio_dir_ = self.get_parameter('audio_dir').get_parameter_value().string_value
+        dtype = self.get_parameter('data_type').get_parameter_value().string_value
         
         self.publisher_ = self.create_publisher(String, 'whisper_output', 10)
         self.subscriber_ = self.create_subscription(Int8, 'whisper_input', self.get_command_callback, 10)
-        
-        # load whisper model
+
+        dtype_map = {
+            'float16': torch.float16,
+            'float32': torch.float32,
+            'float64': torch.float64,
+        }
+
+        torch_dtype = dtype_map.get(dtype, torch.float16)
+
         try:
             self.asr_pipeline_ = pipeline(
                 'automatic-speech-recognition',
                 model=self.model_name_,
-                torch_dtype=torch.float16,
-                device=self.device_,
+                torch_dtype=torch_dtype,
+                device=self.get_parameter('device').get_parameter_value().string_value if torch.cuda.is_available() else 'cpu',
                 model_kwargs={'attn_implementation': 'flash_attention_2'} if is_flash_attn_2_available() else {'attn_implementation': 'sdpa'},
             )
             
             self.get_logger().info("Loaded model %s" % self.model_name_)
+            self.get_logger().info("Node whisper_node has current state active. Waiting for audio input.")
+
         except:
             self.get_logger().error("Failed to load model %s" % self.model_name_)
             raise Exception("Failed to load model %s" % self.model_name_)
@@ -38,22 +59,22 @@ class WhisperNode(Node):
     def get_command_callback(self, incoming_msg: Int8) -> None:
         try:
             if incoming_msg.data == 1:
-                audio_data = '/home/stinky/ros2_ws/src/nlp_handler/audio_files/1.wav'
+                audio_data = os.path.join(self.audio_dir_, str(incoming_msg.data) + '.wav')
             elif incoming_msg.data == 2:
-                audio_data = '/home/stinky/ros2_ws/src/nlp_handler/audio_files/2.wav'
+                audio_data = os.path.join(self.audio_dir_, str(incoming_msg.data) + '.wav')
             elif incoming_msg.data == 3:
-                audio_data = '/home/stinky/ros2_ws/src/nlp_handler/audio_files/3.wav'
+                audio_data = os.path.join(self.audio_dir_, str(incoming_msg.data) + '.wav')
             elif incoming_msg.data == 4:
-                audio_data = '/home/stinky/ros2_ws/src/nlp_handler/audio_files/4.wav'
+                audio_data = os.path.join(self.audio_dir_, str(incoming_msg.data) + '.wav')
         except:
-            self.get_logger().error("No such audio file %i." % incoming_msg.data)
+            self.get_logger().error("No such audio file %i.wav" % incoming_msg.data)
             return
         
         self.get_logger().info("Transcribing audio file %i.wav" % incoming_msg.data)
         output = self.asr_pipeline_(
             audio_data,
-            chunk_length_s=30,
-            batch_size=24,
+            chunk_length_s=self.get_parameter('chunk_length').get_parameter_value().integer_value,
+            batch_size=self.get_parameter('batch_size').get_parameter_value().integer_value,
             return_timestamps=True,
         )
         self.get_logger().info("Transcription complete.")
